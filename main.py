018a9a1795c7a2c4a5d7cc3a3968f74e2a668005
Maks Naskret <maksymiliannaskret@gmail.com>
2025-11-16 23:54:28 +0100

    nothing

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ import json
 
 with open("info.json", "r") as f:
     info = json.load(f)
+    
 
-print(info)
+hoursInDay = info["hoursInDay"]
+worstCaseSolar = info["worstCaseSolar"]
+for i in range(hoursInDay - worstCaseSolar):
+    workingHours = i + worstCaseSolar
+    paidHours = hoursInDay-workingHours
+    print(paidHours, hoursInDay, workingHours)
